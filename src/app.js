dd7a8e37be1dd4b667650ed46cd60a9a44851e82
Diego Soto <juandisoca@gmail.com>
2023-02-04 00:33:39 -0600

    funcionalidad de checkeo de form lista

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+$(".form-control").blur(function () {
+  if (!$(this).val()) { //Si el campo no esta vacio
+    $(this).addClass("error");
+  } else {
+    $(this).removeClass("error");
+    $(this).addClass("correcto");
+  }
+})
+
+// Sweet alert, form enviado
 $("#submit-form").click(() => {
   swal({
     title: "Gracias por enviar el fomurlario",
