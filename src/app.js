1d46d6fefac0702f3853929f2c29420ce97df7bd
Diego <juandisoca@gmail.com>
2023-02-12 10:49:15 -0600

    Sugerencias agregadas

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// Funcionalidad 1
 $(".form-control").blur(function () {
   if (!$(this).val()) { //Si el campo no esta vacio
     $(this).addClass("error");
@@ -7,21 +8,50 @@ $(".form-control").blur(function () {
   }
 })
 
-// Sweet alert, form enviado
+// Funcionalidad 2 y 3
 $("#submit-form").click(() => {
-  swal({
-    title: "Gracias por enviar el fomurlario",
-    text: "Esperamos poder responderte pronto",
-    icon: "success",
-  });
-  event.preventDefault();
+  var name = $("#name").val();
+  var email = $("#email").val();
+  var subject = $("#subject").val();
+  var message = $("#message").val();
+
+  var fill = '';
+
+  if (name == '' || subject == '' || email == '' || message == '') {
+    if (name == '') {
+      fill += "Nombre "
+    }
+    if (subject == '') {
+      fill += "Apellido "
+    }
+    if (email == '') {
+      fill += "E-Mail "
+    }
+    if (message == '') {
+      fill += "Teléfono "
+    }
+    swal({
+      title: "Ooops, no se ha llenado el formulario",
+      text: "Favor llenar lo siguente: " + fill,
+      icon: "warning",
+    });
+    event.preventDefault();
+
+  } else {
+    swal({
+      title: "Formulario enviado",
+      icon: "success",
+    });
+    event.preventDefault();
+  }
 })
 
 
 
+// Funcionalidad 4 
+$(".card-pic").click(() => {
+
+})
 
 
-
-
-
-
+// TODO: Tooltip
